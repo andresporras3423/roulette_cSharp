ef0fa8db8e02c314e5ce628d89caa261ef496632
oscarporras02 <oscarrussi@outlook.com>
2021-01-22 05:00:41 -0500

    add message explaining game status after the roulette is closed

--- roulette_cSharp/Controllers/BetController.cs
+++ roulette_cSharp/Controllers/BetController.cs
@@ -17,6 +17,7 @@ namespace roulette_cSharp.Controllers
     {
         readonly Connection c = new Connection();
         [HttpPost]
+        //bet_number: from 0 until 36 for specific number bet, 37 for odd numbers, 38 for even numbers
         public string Post([FromBody] object response)
         {
             JObject json_params = JObject.Parse(response.ToString());
@@ -24,7 +25,6 @@ namespace roulette_cSharp.Controllers
             {
                 update_player_credit((String)json_params["id_player"], -1 * (Double)json_params["value"]);
                 var bets = c.Database.GetCollection<Bet>("bet");
-                //bet_number: from 0 until 36 for specific number bet, 37 for odd numbers, 38 for even numbers
                 Bet nBet = new Bet { Id_game = (String)json_params["id_game"], Id_player = (String)json_params["id_player"], Bet_number = (Int32)json_params["bet_number"], Value = (Double)json_params["value"] };
                 bets.InsertOne(nBet);
 
@@ -37,7 +37,7 @@ namespace roulette_cSharp.Controllers
             }
         }
         [HttpPut]
-        public bool Put(string idGame)
+        public string Put(string idGame)
         {
             try
             {
@@ -45,29 +45,43 @@ namespace roulette_cSharp.Controllers
                 int winner_number = new Random().Next(0, 36);
                 var filter = Builders<Bet>.Filter.Eq("id_game", idGame);
                 var bets = c.Database.GetCollection<Bet>("bet").Find(filter).ToList();
-                give_prize_to_winner(bets, winner_number);
-                //c.Database.RunCommand(new JsonCommand<BsonDocument>(""));
-
-                return true;
+                JObject json_data = give_prize_to_winner(bets, winner_number);
+                return json_data.ToString();
             }
             catch
             {
 
-                return false;
+                return "-1";
             }
         }
-
-        private void give_prize_to_winner(List<Bet> bets, int winner_number)
+        private JObject give_prize_to_winner(List<Bet> bets, int winner_number)
         {
             JObject message = new JObject();
+            JObject bets_data = new JObject();
             message.Add("winner_number", winner_number);
-            foreach (var b in bets)
+            for(int i=0; i< bets.Count; i++)
             {
                 float increase_factor = 0f;
-                if ((b.Bet_number == 38 && winner_number % 2 == 0) || (b.Bet_number == 37 && winner_number % 2 == 1)) increase_factor = 1.8f;
-                else if (b.Bet_number == winner_number) increase_factor = 5f;
-                update_player_credit(b.Id_player, b.Value * increase_factor);
+                if ((bets[i].Bet_number == 38 && winner_number % 2 == 0) || (bets[i].Bet_number == 37 && winner_number % 2 == 1)) increase_factor = 1.8f;
+                else if (bets[i].Bet_number == winner_number) increase_factor = 5f;
+                update_player_credit(bets[i].Id_player, bets[i].Value * increase_factor);
+                JObject current_bet = add_data_current_bet(bets[i], increase_factor);
+                bets_data.Add(bets[i].Id.ToString(), current_bet);
             }
+            message.Add("bets", bets_data);
+
+            return message;
+        }
+        private JObject add_data_current_bet(Bet bet_data, float increase_factor)
+        {
+            JObject current_bet = new JObject();
+            current_bet.Add("id_player", bet_data.Id_player.ToString());
+            string bet_current_player = bet_data.Bet_number < 37 ? bet_data.Bet_number.ToString() : (bet_data.Bet_number == 37 ? "Black" : "Red");
+            current_bet.Add("bet", bet_current_player);
+            current_bet.Add("status", increase_factor == 0f ? "Lose" : "Win");
+            current_bet.Add("prize", increase_factor == 0f ? 0 : bet_data.Value * increase_factor);
+
+            return current_bet;
         }
         private void close_game(string id)
         {
