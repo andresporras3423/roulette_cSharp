137f96038d57a9eb6fe61343deb24dc782c633df
oscarporras02 <oscarrussi@outlook.com>
2021-03-19 10:30:35 -0500

    add method to get top 100 pokemons

---
+++ roulette_cSharp/Controllers/PokemonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roulette_cSharp.Controllers
+{
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("pokemon/get")]
+        public IActionResult get()
+        {
+            string parent_path = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).FullName;
+            string pokemons_file = Path.Combine(parent_path, "roulette_cSharp\\texts\\pokemons.txt");
+            string text = System.IO.File.ReadAllText(pokemons_file);
+            return Ok(text);
+        }
+    }
+}
